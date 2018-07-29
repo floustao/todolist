9ac89bdfeb845b2ac7423bfec999eca48d5bfa3b
Florestan Lefevre <florestanlefevre@gmail.com>
2018-07-28 21:16:15 -0700

    Create TodoList component and basic styling

---
+++ src/TodoList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items:[]
+    }
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    if (this.myRef.value !== "") {
+      let newTask = {
+        text: this.myRef.value,
+        key: Date.now()
+      };
+      this.setState((prevState) => {
+        return {items: prevState.items.concat(newTask)};
+      });
+    }
+    this.myRef.value = "";
+    console.log(this.state.items);
+  }
+
+  render() {
+    return (
+
+      <div className="main">
+        <h1>Todo App</h1>
+        <div className="main-form">
+          <form onSubmit={this.addItem}>
+            <input type="text" placeholder="add a task" ref={ (a) => this.myRef = a }/>
+            <button type="submit">+</button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+export default TodoList;
