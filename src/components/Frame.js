4cc0de6e7b0227e5bcfe79df1b2e3f95fea391db
Florestan Lefevre <florestanlefevre@gmail.com>
2018-07-30 21:11:16 -0700

    Fix sass config

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -5,6 +5,38 @@ class Frame extends Component {
     return (
       <div className="calculator">
         <h1>Calculator</h1>
+        <form>
+          <div className="screen-container">
+            <input type="text" placeholder="input"/>
+            <input type="text" placeholder="output" readOnly/>
+          </div>
+          <div className="buttons">
+            <div className="row-1">
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+              <button>/</button>
+            </div>
+            <div className="row-2">
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+              <button>-</button>
+            </div>
+            <div className="row-3">
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>+</button>
+            </div>
+            <div className="row-4">
+              <button>0</button>
+              <button>.</button>
+              <button>=</button>
+              <button>*</button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './components/TodoList';
 import Frame from './components/Frame';
-import './styling/index.css';
+import './styles/index.css';
 
 ReactDOM.render(<TodoList />, document.getElementById('todoApp'));
 ReactDOM.render(<Frame />, document.getElementById('calculatorApp'));
