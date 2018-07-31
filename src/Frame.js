81fd41feedf338ff73303f5d5041870e7d83bccd
Florestan Lefevre <florestanlefevre@gmail.com>
2018-07-30 18:42:04 -0700

    Start calculator

---
+++ src/Frame.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Frame extends Component {
+  render() {
+    return (
+      <div className="calculator">
+        <h1>Calculator</h1>
+      </div>
+    );
+  }
+}
+
+export default Frame;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './TodoList';
+import Frame from './Frame';
 import './index.css';
+import './todoApp.css';
 
-ReactDOM.render(<TodoList />, document.getElementById('container'));
+ReactDOM.render(<TodoList />, document.getElementById('todoApp'));
+ReactDOM.render(<Frame />, document.getElementById('calculatorApp'));
+
