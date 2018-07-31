bd5ab58a921b50949ee9f9c3a368ca3988102927
Florestan Lefevre <florestanlefevre@gmail.com>
2018-07-30 19:17:41 -0700

    Reorganize folders and files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
-import Frame from './Frame';
-import './index.css';
+import TodoList from './components/TodoList';
+import Frame from './components/Frame';
+import './styling/index.css';
 
 ReactDOM.render(<TodoList />, document.getElementById('todoApp'));
 ReactDOM.render(<Frame />, document.getElementById('calculatorApp'));
